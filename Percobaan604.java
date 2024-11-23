b7e957430655117d0b12e0c9921aa7eec6a378fc
bungaauulia <bungaaulia1215@gmail.com>
2024-11-23 22:30:19 +0700

    commit hitung luas volume

--- Percobaan604.java
+++ Percobaan604.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 public class Percobaan604 {
+    static int hitungLuas (int pjg, int lb) {
+        int Luas =  pjg*lb;
+        return Luas;
+    }
+    static int hitungVolume (int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int p,l,t,L,vol;
+        int p,l,t;
         System.out.print("Masukkan panjang: ");
         p = input.nextInt();
         System.out.print("Masukkan lebar: ");
@@ -11,9 +19,7 @@ public class Percobaan604 {
         System.out.print("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p*l;
-        System.out.println("Luas persegi panjang adalah " + L);
-        vol = p*l*t;
-        System.out.println("Volume balok adalah " + vol);
+        System.out.println("Luas persegi panjang adalah " + hitungLuas(p, l));
+        System.out.println("Volume balok adalah " + hitungVolume(p, l, t));
     }
 }
